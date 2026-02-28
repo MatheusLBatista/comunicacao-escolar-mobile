4d55251438dcb41547be50bd20b3cf3ab032841a
Matheus Batista <matheusifro2020@gmail.com>
2026-02-27 21:04:03 -0400

    chore: criando arquivo DailyLogsScreen e inicializando app no emulador

--- app/src/main/java/dev/fslab/comunicacao/escolar/MainActivity.kt
+++ app/src/main/java/dev/fslab/comunicacao/escolar/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.fslab.comunicacao.escolar.ui.theme.ComunicacaoEscolarTheme
+import dev.fslab.comunicacao.escolar.ui.theme.screens.DailyLogsScreen
 import dev.fslab.comunicacao.escolar.ui.theme.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
@@ -46,8 +47,12 @@ fun ComunicacaoEscolarApp() {
                         navController.navigate("esqueci_senha?email=$email")
                     },
                     onRegister = { navController.navigate("cadastro") },
-                    onLogin = { navController.navigate("home") }
+                    onLogin = { navController.navigate("dailyLogs") }
                 )
+            }
+
+            composable("dailyLogs") {
+                DailyLogsScreen()
             }
 //            composable(
 //                route = "esqueci_senha?email={email}",
---
+++ app/src/main/java/dev/fslab/comunicacao/escolar/ui/theme/screens/DailyLogsScreen.kt
@@ -0,0 +1,72 @@
+package dev.fslab.comunicacao.escolar.ui.theme.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.fslab.comunicacao.escolar.ui.theme.ComunicacaoEscolarTheme
+import dev.fslab.comunicacao.escolar.ui.theme.LocalComunicacaoEscolarColors
+
+@Composable
+fun DailyLogsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LocalComunicacaoEscolarColors.current.background)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Daily Logs Screen",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
